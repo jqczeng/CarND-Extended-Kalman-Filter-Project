dd425530c46341c2563c543784a1511191f08911
Jason Zeng <jzeng@Jasons-MacBook-Pro.local>
2019-02-27 17:04:19 -0500

    fixed Hj matrix calculation

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,7 +15,7 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-   * TODO: Calculate the RMSE here.
+   *  Calculate the RMSE here.
    */
 
 	VectorXd rmse(4);
@@ -24,15 +24,15 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	// check the validity of the following inputs:
 	//  * the estimation vector size should not be zero
 	//  * the estimation vector size should equal ground truth vector size
-	if (estimations.size() == 0 || estimations.size() != ground_truth.size()){
-	cout << "Error: Estimations Size Wrong or Ground Truth Size Wrong" << endl;
+	if (estimations.size() == 0 || ground_truth.size() == 0 || estimations.size() != ground_truth.size()){
+		cout << "Error: Estimations Size Wrong or Ground Truth Size Wrong" << endl;
 	}
 
 	// Accumulate squared residuals
 	for (int i=0; i < estimations.size(); ++i) {
-	VectorXd residual= estimations[i] - ground_truth[i];
-	residual = residual.array().pow(2);
-	rmse += residual;
+		VectorXd residual= estimations[i] - ground_truth[i];
+		residual = residual.array().pow(2);
+		rmse += residual;
 	}
 
 	// Calculate the mean
@@ -60,7 +60,7 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
 	float vx = x_state(2);
 	float vy = x_state(3);
 
-	// check division by zero
+//	// check division by zero
 	if (fabs(px*px + py+py) < 0.0001) {
 	    cout << "CalculateJacobian () - Error - Division by Zero" << endl;
 	    return Hj;
@@ -74,14 +74,15 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
 		float Hj_11 = px / (px*px + py*py);
 		float Hj_12 = 0;
 		float Hj_13 = 0;
-		float Hj_20 = py * (vx*py - vy*px) / pow(px*px+py*py, 3/2);
-		float Hj_21 = px * (vy*px - vx*py) / pow(px*px+py*py, 3/2);
+		float Hj_20 = py * (vx*py - vy*px) / pow(px*px+py*py, 1.5);
+		float Hj_21 = px * (vy*px - vx*py) / pow(px*px+py*py, 1.5);
 		float Hj_22 = px / sqrt(px*px + py*py);
 		float Hj_23 = py / sqrt(px*px + py*py);
 
 	Hj << Hj_00, Hj_01, Hj_02, Hj_03,
 		  Hj_10, Hj_11, Hj_12, Hj_13,
 		  Hj_20, Hj_21, Hj_22, Hj_23;
+
 	}
 
 	return Hj;
